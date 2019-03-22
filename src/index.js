6d681da9440aa174e2474f359a97e8a0c131cefa
ollie <oleg.laliev.developer@gmail.com>
2019-03-22 20:19:13 +0300

    feat: new completed task - brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length%2 !== 0) {
+    return false
+  }
+
+  let closers_brace = [];
+  let opens_brace = [];
+  for( let j=0; j<bracketsConfig.length; j++){
+    opens_brace.push(bracketsConfig[j][0]);
+    closers_brace.push(bracketsConfig[j][1])
+  }
+
+  let order = [];
+  for (let i = 0; i < str.length; i++) {
+    if(opens_brace.indexOf(str[i]) !== -1) {
+      if (opens_brace.indexOf(str[i])===closers_brace.indexOf(str[i]) && str[i] === order[order.length-1]) {
+        order.pop();
+      } else {
+        order.push(str[i]);
+      }
+    }
+    else if (closers_brace.indexOf(str[i]) !== -1 && closers_brace.indexOf(str[i]) === opens_brace.indexOf(order[order.length-1])) {
+        order.pop();
+      }
+      else {
+        return false;
+      }
+    }
+
+  if (order.length > 0) return false;
+
+  return true
 }
